b12678e16977db40f5e0b181df84059282faca31
hyth32 <5474n.sage@gmail.com>
2024-04-20 13:05:46 +0300

    added lw7p1

---
+++ lw7/task1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Input file path:");
+            string filePath = Console.ReadLine();
+
+            Console.WriteLine("Input chars to remove (w/o spaces):");
+            string charsToRemove = Console.ReadLine();
+
+            var strBuilder = new StringBuilder();
+
+            using (var reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    foreach (char c in charsToRemove)
+                    {
+                        line = line.Replace(c.ToString(), "");
+                    }
+                    strBuilder.AppendLine(line);
+                }
+            }
+
+            using (var writer = new StreamWriter(filePath))
+            {
+                await writer.WriteAsync(strBuilder.ToString());
+            }
+
+            Console.WriteLine("Изменения сохранены.");
+            Console.ReadLine();
+        }
+    }
+}
